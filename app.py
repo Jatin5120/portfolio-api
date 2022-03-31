4ca4f760e5a72da9d23261a81f6c1424feb16196
Jatin <getjatin5120@gmail.com>
2022-03-31 18:20:32 +0530

    Fixing bug: variables not defined

--- app.py
+++ app.py
@@ -4,17 +4,7 @@ import json
 app = Flask(__name__)
 
 
-def setData():
-    global allData, projects
-
-    data = open("all.json", "r")
-    allData = json.load(data)
-
-    data = open("projects.json", "r")
-    projects = json.load(data)
-
-
-def getError(error="Invalid request", message="Unknown error occured."):
+def _getError(error="Invalid request", message="Unknown error occured."):
     return jsonify({"error": error, "message": message})
 
 
@@ -22,7 +12,8 @@ def getError(error="Invalid request", message="Unknown error occured."):
 @app.route("/home/")
 @app.route("/dashboard/")
 def index():
-    global allData
+    data = open("all.json", "r")
+    allData = json.load(data)
 
     return jsonify(allData)
 
@@ -30,27 +21,26 @@ def index():
 @app.route("/project/")
 @app.route("/project/<projectId>/")
 def project(projectId=None):
-    global projects
+    data = open("projects.json", "r")
+    projects = json.load(data)
 
     if projectId is None:
-        return getError(error="Invalid request", message="Project ID is missing")
+        return _getError(error="Invalid request", message="Project ID is missing")
 
     for project in projects:
         if project["id"] == projectId:
 
             return jsonify(project)
 
-    return getError(error="Wrong Project ID", message="Project not found")
+    return _getError(error="Wrong Project ID", message="Project not found")
 
 
 @app.route("/<route>/")
 @app.route("/<route>/<projectId>/")
 def noRoute(route, projectId=None):
-    return getError(error="Invalid route", message=f"Route '{route}' not found")
+    return _getError(error="Invalid route", message=f"Route '{route}' not found")
 
 
 if __name__ == "__main__":
-    allData, projects = {}, []
-    setData()
 
     app.run(debug=True)
